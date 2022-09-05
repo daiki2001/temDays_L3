479a275c0a41d79d717dc9dc7ede0a5f46198971
エグチダイキ <daiki20011222@gmail.com>
2022-09-05 15:42:42 +0900

    DxLibの必須関数の追加

--- tenDays/main.cpp
+++ tenDays/main.cpp
@@ -1,6 +1,68 @@
 #include <DxLib.h>
 
+// ウィンドウのタイトルに表示する文字列
+const char TITLE[] = "title";
+
+// ウィンドウ横幅
+const int WIN_WIDTH = 85 * 16;
+
+// ウィンドウ縦幅
+const int WIN_HEIGHT = 85 * 9;
+
 int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPSTR lpCmdLine, _In_ int nCmdShow)
 {
+	// ウィンドウモードに設定
+	ChangeWindowMode(TRUE);
+
+	// ウィンドウサイズを手動では変更させず、
+	// かつウィンドウサイズに合わせて拡大できないようにする
+	SetWindowSizeChangeEnableFlag(FALSE, FALSE);
+
+	// タイトルを変更
+	SetMainWindowText(TITLE);
+
+	// 画面サイズの最大サイズ、カラービット数を設定(モニターの解像度に合わせる)
+	SetGraphMode(WIN_WIDTH, WIN_HEIGHT, 32);
+
+	// 画面サイズを設定(解像度との比率で設定)
+	SetWindowSizeExtendRate(1.0);
+
+	// 画面の背景色を設定する
+	SetBackgroundColor(0x00, 0x00, 0xFF);
+
+	// DXlibの初期化
+	if (DxLib_Init() == -1) { return -1; }
+
+	// (ダブルバッファ)描画先グラフィック領域は裏面を指定
+	SetDrawScreen(DX_SCREEN_BACK);
+
+	while (1)
+	{
+		// 画面クリア
+		ClearDrawScreen();
+
+
+
+		// (ダブルバッファ)裏面
+		ScreenFlip();
+
+		// 20ミリ秒待機(疑似60FPS)
+		WaitTimer(20);
+
+		// Windowsシステムからくる情報を処理する
+		if (ProcessMessage() == -1)
+		{
+			break;
+		}
+
+		// ESCキーが押されたらループから抜ける
+		if (CheckHitKey(KEY_INPUT_ESCAPE) == TRUE)
+		{
+			break;
+		}
+	}
+	// Dxライブラリ終了処理
+	DxLib_End();
+
 	return 0;
 }
