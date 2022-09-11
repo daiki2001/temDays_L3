e0c632cf7bf3fabebbbd081c69410bdc221f38bd
エグチダイキ <daiki20011222@gmail.com>
2022-09-11 18:44:18 +0900

    ブロックに画像適応 + ゴールしてもプレイヤーが動き続けるようにした

--- tenDays/Stage.cpp
+++ tenDays/Stage.cpp
@@ -1,4 +1,4 @@
-#include "Stage.h"
+﻿#include "Stage.h"
 
 Stage::Stage()
 {
@@ -16,6 +16,7 @@ Stage::~Stage()
 void Stage::Init()
 {
 	stageNum = 1;
+	Load();
 	CreateStage();
 }
 
@@ -46,17 +47,60 @@ void Stage::Update()
 
 void Stage::Draw()
 {
+	static const int graphSize = 128;
+
 	for (int i = 0; i < boxData.size(); i++)
 	{
-		DrawBox(boxData[i]->pos.x - boxData[i]->size.x, boxData[i]->pos.y - boxData[i]->size.y,
-			boxData[i]->pos.x + boxData[i]->size.x, boxData[i]->pos.y + boxData[i]->size.y,
-			GetColor(255, 255, 255), true);
+		//DrawBox(boxData[i]->pos.x - boxData[i]->size.x, boxData[i]->pos.y - boxData[i]->size.y,
+		//	boxData[i]->pos.x + boxData[i]->size.x, boxData[i]->pos.y + boxData[i]->size.y,
+		//	GetColor(255, 255, 255), true);
+
+		for (size_t y = 0; y * graphSize < boxData[i]->size.y * 2; y++)
+		{
+			int up = (boxData[i]->pos.y - boxData[i]->size.y) + (y * graphSize);
+			bool isRect = (up + graphSize) > (boxData[i]->pos.y + boxData[i]->size.y);
+
+			for (size_t x = 0; x * graphSize < boxData[i]->size.x * 2; x++)
+			{
+				int left = (boxData[i]->pos.x - boxData[i]->size.x) + (x * graphSize);
+				isRect |= (left + graphSize) > (boxData[i]->pos.x + boxData[i]->size.x);
+
+				if (isRect)
+				{
+					int width = graphSize;
+					int height = graphSize;
+
+					if ((left + graphSize) > static_cast<int>(boxData[i]->pos.x + boxData[i]->size.x))
+					{
+						width -= (left + graphSize) - static_cast<int>(boxData[i]->pos.x + boxData[i]->size.x);
+					}
+					if ((up + graphSize) > static_cast<int>(boxData[i]->pos.y + boxData[i]->size.y))
+					{
+						height -= (up + graphSize) - static_cast<int>(boxData[i]->pos.y + boxData[i]->size.y);
+					}
+
+					DrawRectGraph(left, up, 0, 0, width, height, cubeBlock, true);
+				}
+				else
+				{
+					DrawGraph(left, up, cubeBlock, true);
+				}
+			}
+		}
 	}
 }
 
 void Stage::StageAddOne()
 {
 	stageNum++;
+}
+
+void Stage::Load()
+{
+	if (cubeBlock == -1)
+	{
+		cubeBlock = LoadGraph("./Resources/block/cube/groundcube1.png");
+	}
 }
 
 void Stage::StageOne()
