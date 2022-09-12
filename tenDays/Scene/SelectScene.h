e73f16526a84c4f297fd3cafdd287440407442ba
エグチダイキ <daiki20011222@gmail.com>
2022-09-12 13:17:54 +0900

    操作棒の画像変更

---
+++ tenDays/Scene/SelectScene.h
@@ -0,0 +1,21 @@
+﻿#pragma once
+#include "BaseScene.h"
+
+class SelectScene : public BaseScene
+{
+public: //メンバ関数
+	SelectScene(SceneChenger* sceneChenger);
+	~SelectScene() override;
+
+	// 初期化処理
+	void Init() override;
+	// 更新処理
+	void Update() override;
+	// 描画処理
+	void Draw() override;
+
+	// 必要なファイルを全て読み込み
+	void Load() override;
+	// 読み込んだファイルを全て開放
+	void Release() override;
+};
